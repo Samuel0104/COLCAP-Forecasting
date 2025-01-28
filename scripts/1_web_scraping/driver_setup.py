09271d1f1bb272f413830b73aa862779a07e2283
Samuel0104 <samuelmorenovahos@hotmail.com>
2025-01-28 17:18:19 +0000

    Create driver_setup.py

---
+++ scripts/1_web_scraping/driver_setup.py
@@ -0,0 +1,25 @@
+# General libraries
+import time
+import pandas as pd
+
+# Web scraping
+import requests
+import bs4
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+
+# System
+import os
+import urllib3
+os.environ["CURL_CA_BUNDLE"] = ""
+urllib3.disable_warnings()
+
+# Driver setup
+opts = webdriver.ChromeOptions()
+opts.add_argument("--headless")
+opts.add_argument("--no-sandbox")
+opts.add_argument("--disable-dev-shm-usage")
+prefs = {"profile.managed_default_content_settings.images": 2}
+opts.add_experimental_option("prefs", prefs)
+driver = webdriver.Chrome(options=opts)
